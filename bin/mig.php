8d76788b6a6ffff320d62dc2f0f510dae24122a5
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-12-05 18:55:55 -0300

    add bin/mig.php

--- bin/mig.php
+++ bin/mig.php
@@ -1,1 +1,10 @@
 <?php
+require __DIR__.'/../cfg.php';
+
+use gaucho\mig;
+
+$pdo=$db->pdo;
+$dbType='sqlite';
+$tableDirectory=glob($_ENV['SITE_ROOT'].'/hmvc/*/table');
+$mig=new mig($pdo,$tableDirectory,$dbType);
+$mig->mig();
