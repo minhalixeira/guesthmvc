5f07714e9a5df1ed3ec4a443408864dcd19ad60f
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-12-12 15:34:09 -0300

    ajustar o tamanho do grid

--- hmvc/home/view/home.php
+++ hmvc/home/view/home.php
@@ -1,6 +1,7 @@
 <div class="container">
 	<div class="row">
-		<div class="col12 text-center">
+		<div class="col3"></div>
+		<div class="col6 text-center">
 			<h1>Guest HMVC</h1>
 			<?php 
 			$action=$_ENV['SITE_URL'].'/messages';
